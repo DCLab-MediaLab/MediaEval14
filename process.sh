631e798673e51472433bb1e02dbf1f8c5ec2ea91
Bálint Fodor <balint.fodor@gmail.com>
2014-07-02 19:56:03 +0200

    added uber process.sh, renamed webdata_converter

---
+++ process.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; do
+    echo "Usage $0 <input dir> <output dir>"
+    exit -1
+done
+
+inp=$1
+out=$2
+
+php converter/json/json_convert.php $inp $out
+php converter/subtitle/subtitle_convert.php $inp $out
+./converter/tar/tar_convert.sh $inp $out
+cd converter/transcript-LIUM; ./build.sh; ./run.sh ../../$inp ../../$out; cd ../..
