c961edf1adf6c32a387675110d6badc03e4d0459
Bálint Fodor <balint.fodor@gmail.com>
2014-07-03 09:31:16 +0200

    tar to csv, closes #6

--- converter/tar/tar_convert.sh
+++ converter/tar/tar_convert.sh
@@ -14,13 +14,21 @@ mkdir -p $out
 
 for file in $tars; do
     name=$(basename $file | sed "s/v\(.*\)\.tar/\1/")
-    echo $name
+    echo "processing tar $name"
     tar xf $file cSH2013/v$name/info
     tar xf $file cSH2013/v$name/scenecut
-    tar xf $file cSH2013/v$name/stableframe
-    mv cSH2013/v$name/info $out/$name.tar.info.csv
-    mv cSH2013/v$name/scenecut $out/$name.tar.scenecut.csv
-    mv cSH2013/v$name/stableframe $out/$name.tar.stableframe.csv
+
+    infofile="$out/$name.tar.info.csv"
+    echo "filename;length;fps" > $infofile
+    echo -n "$name;" >> $infofile
+    tail -n 2 cSH2013/v$name/info | tr "\n" ";"  >> $infofile
+
+    cutfile="$out/$name.tar.scenecut.csv"
+    echo "filename;frameid" > $cutfile
+    for fr in $(tail -n 1 cSH2013/v$name/scenecut); do
+        echo -n "$name;" >> $cutfile
+        echo "$fr" >> $cutfile
+    done
 done
 
 rm -rf cSH2013
