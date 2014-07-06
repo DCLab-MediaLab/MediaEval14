74afbbb1dd3b98294be0b860b945a0b257311830
Bálint Fodor <balint.fodor@gmail.com>
2014-07-06 16:33:54 +0200

    improving scene cut script

--- cut_into_scenes.py
+++ cut_into_scenes.py
@@ -26,49 +26,57 @@ def process_type(descr_type, filename):
 	print("processing %s %s" % (descr_type, filename))
 	switch = {
 			"subtitle" : 
-				{"cols": ['begin', 'end', 'text'], 
+				{"cols": ['begin', 'end', 'text'],
+				"types": [float, float, str],
 				"time_start": "begin",
 				"time_end": "end"},
 			"transcript-lium":
 				{"cols": ['filename', 'sdr', 'start', 'duration', 'word', 'confidence'],
+				"types": [str, float, float, float, str, float],
 				"time_start": "start",
 				"length": "duration"}
-
-			#"json":	['diskref','service','date','time',
-			#	'duration','uri','canonical','depiction',
-			#	'title','description','original_description',
-			#	'subtitles_uri','when','id','filename',
-			#	'source','service_name']
 			}
 	if descr_type not in switch:
 		print("warn: can not handle descr_type %s" % descr_type)
 	else:
-		process_by_scheme(switch[descr_type], filename)
-		write_time_interval_file()
+		cur = process_by_scheme(switch[descr_type], filename)
+		write_time_interval_file(cur, (30.0, 40.), switch[descr_type], "outt")
+
+def py2sqlite(tplst):
+	tpdict = {float: "REAL", str: "TEXT", int: "INTEGER"}
+	return [tpdict[i] for i in tplst]
 
 def process_by_scheme(scheme, csvfile):
 	con = sqlite3.connect(":memory:")
+	con.text_factory = str
 	cur = con.cursor()
-	cols = ",".join([i.replace(" ", "") for i in scheme])
-	qmarks = ",".join(["?"]*len(scheme))
-	print(cols)
-	cur.execute("CREATE TABLE t (%s);" % cols)
+
+	qmarks = ",".join(["?"]*len(scheme["cols"]))
+	decl = ",".join(["%s %s" % (v, t) for v, t in zip(scheme["cols"], py2sqlite(scheme["types"])) ])
+	cols = ",".join([x for x in scheme["cols"]])
+
+	cur.execute("CREATE TABLE t (%s);" % decl)
 
 	with open(csvfile, 'rb') as fin:
 	    dr = csv.DictReader(fin, delimiter=";")
-	    to_db = [tuple([i[j] for j in scheme]) for i in dr]
-
-	print(to_db[1])
+	    to_db = [tuple([typ(row[col]) for col, typ in zip(scheme["cols"], scheme["types"])]) for row in dr]
 
 	cur.executemany("INSERT INTO t (%s) VALUES (%s);" % (cols, qmarks), to_db)
 	con.commit()
 
+	print(to_db[0])
+
 	return cur
 	# dispose connection
 
-
 def write_time_interval_file(cur, interval, scheme, out_dir):
-	cur.execute("SELECT * FROM t WHERE ")
+	if "time_end" in scheme:
+		print("SELECT * FROM t WHERE %s<'%s' AND '%s'<%s"
+			% (interval[0], scheme["time_start"], scheme["time_end"], interval[1]))
+		cur.execute("SELECT * FROM t WHERE %s<t.'%s' AND t.'%s'<%s ORDER BY t.'%s'"
+			% (interval[0], scheme["time_start"], scheme["time_end"], interval[1], scheme["time_start"]))
+		data = cur.fetchall()
+		print(data)
 
 if __name__ == "__main__":
 	main()
