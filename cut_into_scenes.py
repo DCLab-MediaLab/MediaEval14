218b698efcb91ab7a0c28d34c6bacbf557ef0d91
Bálint Fodor <balint.fodor@gmail.com>
2014-07-14 08:24:23 +0200

    generating files

--- cut_into_scenes.py
+++ cut_into_scenes.py
@@ -1,5 +1,23 @@
 import csv, sqlite3, glob, sys, os
 
+scheme_switch = {
+		"subtitle" : 
+			{"cols": ['begin', 'end', 'text'],
+			"types": [float, float, str],
+			"time_start": "begin",
+			"time_end": "end"},
+		"transcript-lium":
+			{"cols": ['filename', 'sdr', 'start', 'duration', 'word', 'confidence'],
+			"types": [str, float, float, float, str, float],
+			"time_start": "start",
+			"length": "duration"},
+		"tar.scenecut":
+			{"cols": ['filename', 'frameid'],
+			"types": [str, int],
+			"time_start": "start",
+			"length": "duration"}
+		}
+
 def main():
 
 	if len(sys.argv) < 2:
@@ -10,20 +28,25 @@ def main():
 
 	print("input dir: %s" % inp)
 
+	db_con, db_cur = init_db()
+
+	# enumerating the base video names
 	flst = glob.glob("%s/*.tar.info.csv" % inp)
 
 	for f in flst:
 		base = os.path.split(f)[-1]
 		base = base.split(".")[0]
 		print("processing %s" % base)
+		# using the video names enumerating the feature csv files like transcript.csv etc.
 		csvfiles = glob.glob("%s/%s.*.csv" % (inp, base))
 
-		# hell yeah, syntactic anti-sugar to the rescue
+		# hell yeah, syntactic anti-sugar to the rescue - building a dictionary "filetype" -> "filename"
 		proc_dict = {".".join(os.path.split(descr)[-1].split(".")[1:-1]) : descr
 			for descr in csvfiles}
 
 		cut_points = []
 
+		# reading the scenecut points
 		if "tar.info" in proc_dict and "tar.scenecut" in proc_dict:
 			with open(proc_dict["tar.info"]) as info_file, open(proc_dict["tar.scenecut"]) as cut_file:
 				infocsv = csv.DictReader(info_file, delimiter=";")
@@ -40,72 +63,69 @@ def main():
 		del proc_dict["tar.scenecut"]
 
 		for k in proc_dict.keys():
-			build_type(k, proc_dict[k])
-			#for i in range(len(cut_points)-1):
-			#	f0, f1 = cut_points[i], cut_points[i+1]
-			#	print("cutting interval %f - %f" % (f0, f1))
-			#	process_type(k, proc_dict[k], (f0, f1), "outtt")
+			# csv -> sqlite3
+			if k not in scheme_switch:
+				print("warn: can not handle descr_type %s" % k)
+			else:
+				db_append_by_scheme(k, scheme_switch[k], proc_dict[k], db_con, db_cur)
+				for i in range(len(cut_points)-1):
+					f0, f1 = cut_points[i], cut_points[i+1]
+					write_time_interval_file(base, k, scheme_switch[k], (f0, f1), db_con, db_cur, "outt", i)
 
-def build_type(descr_type, filename):
-	print("processing %s %s" % (descr_type, filename))
-	switch = {
-			"subtitle" : 
-				{"cols": ['begin', 'end', 'text'],
-				"types": [float, float, str],
-				"time_start": "begin",
-				"time_end": "end"},
-			"transcript-lium":
-				{"cols": ['filename', 'sdr', 'start', 'duration', 'word', 'confidence'],
-				"types": [str, float, float, float, str, float],
-				"time_start": "start",
-				"length": "duration"},
-			"tar.scenecut":
-				{"cols": ['filename', 'frameid'],
-				"types": [str, int],
-				"time_start": "start",
-				"length": "duration"}
-			}
-	if descr_type not in switch:
-		print("warn: can not handle descr_type %s" % descr_type)
-	else:
-		cur = build_by_scheme(descr_type, switch[descr_type], filename)
-		#write_time_interval_file(cur, interval, switch[descr_type], out_dir)
+	finit_db(db_con)
+
+def init_db():
+	con = sqlite3.connect(":memory:")
+	con.text_factory = str
+	cur = con.cursor()
+	return con, cur
+
+def finit_db(con):
+	con.close()
 
 def py2sqlite(tplst):
 	tpdict = {float: "REAL", str: "TEXT", int: "INTEGER"}
 	return [tpdict[i] for i in tplst]
 
-def build_by_scheme(descr_type, scheme, csvfile):
-	# csv -> sqlite3 table
-	con = sqlite3.connect(":memory:")
-	con.text_factory = str
-	cur = con.cursor()
+def form_table_name(orig):
+	table_name = orig.replace(".", "_")
+	table_name = table_name.replace("-", "_")
+	return table_name
 
+def db_append_by_scheme(descr_type, scheme, csvfile, db_con, db_cur):
+	# some question marks for the insert query
 	qmarks = ",".join(["?"]*len(scheme["cols"]))
+	# table scheme 
 	decl = ",".join(["%s %s" % (v, t) for v, t in zip(scheme["cols"], py2sqlite(scheme["types"])) ])
 	cols = ",".join([x for x in scheme["cols"]])
+	table_name = form_table_name(descr_type)
 
-	cur.execute("CREATE TABLE t (%s);" % decl)
+	db_cur.execute("CREATE TABLE IF NOT EXISTS %s (%s);" % (table_name, decl))
 
 	with open(csvfile, 'rb') as fin:
 	    dr = csv.DictReader(fin, delimiter=";")
 	    to_db = [tuple([typ(row[col]) for col, typ in zip(scheme["cols"], scheme["types"])]) for row in dr]
 
-	cur.executemany("INSERT INTO t (%s) VALUES (%s);" % (cols, qmarks), to_db)
-	con.commit()
+	# TODO: check if we already have this row in the db
+	db_cur.executemany("INSERT INTO %s (%s) VALUES (%s);" % (table_name, cols, qmarks), to_db)
+	db_con.commit()
 
-	return cur, con
-	# dispose connection
-
-def write_time_interval_file(cur, interval, scheme, out_dir):
+def write_time_interval_file(base, descr_type, scheme, interval, db_con, db_cur, out_dir, cnt):
 	# sql query for filtering time interval
+	table_name = form_table_name(descr_type)
 	if "time_end" in scheme:
-		print("SELECT * FROM t WHERE %s<'%s' AND '%s'<%s"
-			% (interval[0], scheme["time_start"], scheme["time_end"], interval[1]))
-		cur.execute("SELECT * FROM t WHERE %s<t.'%s' AND t.'%s'<%s ORDER BY t.'%s'"
-			% (interval[0], scheme["time_start"], scheme["time_end"], interval[1], scheme["time_start"]))
-		data = cur.fetchall()
-		print(data)
+		query = "SELECT * FROM %s WHERE %s<%s.'%s' AND %s.'%s'<%s ORDER BY %s.'%s'" % (table_name, interval[0], table_name, scheme["time_start"], table_name, scheme["time_end"], interval[1], table_name, scheme["time_start"])
+		db_cur.execute(query)
+		data = db_cur.fetchall()
+		dst = "%s/%s/%s" % (out_dir, base, descr_type)
+		if not os.path.exists(dst):
+			os.makedirs(dst)
+		with open("%s/%s_%s_%03d.csv" % (dst, base, descr_type, cnt), "w") as f:
+			csvhead = ";".join([k for k in scheme["cols"]])
+			f.write(csvhead + "\n")
+			for row in data:
+				f.write(";".join([str(x) for x in row]) + "\n")
+		#print(data)
 
 if __name__ == "__main__":
 	main()
