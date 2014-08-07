1366f67a83c2e2fb8142351e2472b807690998bb
realhidden <zsombor.paroczi@gmail.com>
2014-08-07 23:04:53 +0200

    Anchor subtask

--- submissiongen/submissiongen.php
+++ submissiongen/submissiongen.php
@@ -40,6 +40,17 @@ $transcripts = array(
   Description is a very short for the approach that produced the run ()
  */
 
+function getSubtitleForCut($name) {
+    if (file_exists("cache/e" . md5($name))) {
+        return unserialize(file_get_contents("cache/e" . md5($name)));
+    }
+    $r = json_decode(file_get_contents("http://hiddenmon.cloudapp.net:8983/solr/mm-test/select?q=title:" . urlencode($name) . "&wt=json"), true);
+
+
+    file_put_contents("cache/e" . md5($name), serialize($r));
+    return $r;
+}
+
 function getResultFromSolr($search) {
     if (file_exists("cache/" . md5($search))) {
         return unserialize(file_get_contents("cache/" . md5($search)));
@@ -97,19 +108,19 @@ function getSegmentStartEnd($segment) {
     $lines = explode("\n", file_get_contents("data/times/" . $origfile));
     foreach ($lines as $l1) {
         $vals = explode(",", $l1, 3);
-        if ((count($vals) < 3) || (trim($vals[0])!=$segment))
+        if ((count($vals) < 3) || (trim($vals[0]) != $segment))
             continue;
 
         foreach ($vals as &$v1) {
             $v1 = trim($v1);
         }
-        
-        return array((float)$vals[1],(float)$vals[2]);
+
+        return array((float) $vals[1], (float) $vals[2]);
     }
 
 
     echo("ERROR IN TIMES");
-    return array(0,0);
+    return array(0, 0);
 }
 
 function getTimeFromSeconds($sec) {
@@ -118,7 +129,7 @@ function getTimeFromSeconds($sec) {
     return floor($sec / 60) . "." . ($sec % 60);
 }
 
-function getFormattedResult($res, $identifier, $hasjumpin, $run) {
+function getFormattedResult($res, $identifier, $hasjumpin, $run, $filterone = '') {
     $ret = "";
     //result list
     $rank = 1;
@@ -133,12 +144,15 @@ function getFormattedResult($res, $identifier, $hasjumpin, $run) {
         array_pop($origfile);
         $origfile = implode("_", $origfile);
 
+        if ($origfile == $filterone)
+            continue;
+
         //get the time
         $time = getSegmentStartEnd($oneres);
 
         //10 sec rule
         if ($time[1] - $time[0] < 10)
-            $time[1]=$time[0]+10;
+            $time[1] = $time[0] + 10;
 
         if ($time[1] - $time[0] > 120) {
             $time[1] = $time[0] + 120;
@@ -196,10 +210,9 @@ function searchSubtask() {
                     continue;
                 }
                 $q1 = strtolower(morpho(trim($q1)));
-                if ($transcriptname!='content')
-                {
+                if ($transcriptname != 'content') {
                     $q1 = $transcriptname . "_concept:" . trim($q1) . "";
-                }else{
+                } else {
                     $q1 = $transcriptname . ":" . trim($q1) . "";
                 }
             }
@@ -215,6 +228,105 @@ function searchSubtask() {
     }
 }
 
+function anchorSubtask() {
+    global $transcripts;
+    //load xml
+    $xml = simplexml_load_string(file_get_contents("data/me14sh_linking_testSet_anchors.xml"));
+    $json = json_encode($xml);
+    $array = json_decode($json, TRUE);
+
+    //for each transcript
+    $prio = 1;
+    foreach ($transcripts as $transcriptname => $transcriptcode) {
+        $filename = "me14sh_DCLab2014_L_" . $prio . "_Sh_" . $transcriptcode . "_N_ConceptEnrichment";
+        echo($filename . "\n");
+        $prio++;
+
+        $res = "";
+        //for each query
+        foreach ($array['anchor'] as $anchor) {
+
+            $times = explode("\n", file_get_contents("data/times/" . substr($anchor['fileName'], 1)));
+            $allcuts = array();
+
+            $from = explode(".", $anchor['startTime']);
+            $from = $from[0] * 60 + $from[1];
+
+            $to = explode(".", $anchor['endTime']);
+            $to = $to[0] * 60 + $to[1];
+
+            foreach ($times as $t1) {
+                $vals = explode(",", $t1, 3);
+                if (count($vals) < 3)
+                    continue;
+
+                foreach ($vals as &$v1) {
+                    $v1 = trim($v1);
+                }
+                //getSubtitleForCut(
+
+                for ($i = $from; $i < $to; $i++) {
+                    if (($vals[1] <= $i) && ($i <= $vals[2]) && (!in_array($vals[0], $allcuts)))
+                        $allcuts[] = $vals[0];
+                }
+            }
+
+            //new document from parts
+            $allwords = array();
+            foreach ($allcuts as $c1) {
+                $doc = getSubtitleForCut($c1);
+                $thedoc = $doc['response']['docs'][0];
+
+                //concept / transcript
+                $words = explode(" ", $thedoc[$transcriptname][0]);
+                foreach ($words as $k => $word) {
+                    $word = strtolower($word);
+
+                    if (isStopword($word)) {
+                        unset($words[$k]);
+                        continue;
+                    }
+
+                    $word = morpho($word);
+                    if (isStopword($word)) {
+                        unset($words[$k]);
+                        continue;
+                    }
+
+                    if (in_array($word, $allwords))
+                        continue;
+                    $allwords[] = $word;
+                }
+            }
+
+            //use the words in a query
+            foreach ($words as $k => &$q1) {
+                if (trim($q1) == '') {
+                    unset($q[$k]);
+                    continue;
+                }
+                $q1 = strtolower(morpho(trim($q1)));
+                if ($transcriptname != 'content') {
+                    $q1 = $transcriptname . "_concept:" . trim($q1) . "";
+                } else {
+                    $q1 = $transcriptname . ":" . trim($q1) . "";
+                }
+            }
+            $words=array_slice($words,0,15);
+            $q = implode(" AND ", $words);
+
+            $resSolr = getResultFromSolr($q);
+            $res.=getFormattedResult($resSolr, $anchor['anchorId'], false, $filename, substr($anchor['fileName'], 1));
+
+            //create the query
+        }
+
+        //write to file
+        file_put_contents('result/' . $filename . ".txt", $res);
+    }
+}
+
 genTimes();
-searchSubtask();
+//searchSubtask();
+anchorSubtask();
 ?>
